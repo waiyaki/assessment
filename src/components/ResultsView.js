4455270068dc5cf61d7963e494d0452765a286f8
James Muturi <james.muturi@andela.com>
2017-03-30 14:18:42 +0300

    Create ResultsView component

---
+++ src/components/ResultsView.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const fields = ['title', 'created_at', 'updated_at', 'closed_at']
+
+export const renderFields = (issue) => {
+  return fields.map(field => {
+    const fieldData = issue[field];
+    if (field === 'closed_at') {
+      return <td key={field}>`${fieldData ? 'Closed' : 'Open' }`</td>
+    }
+    return <td key={field}>{fieldData}</td>
+  })
+}
+
+export default function ResultsView({ issues, limit, page }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {fields.map(field => (
+            <th>{field}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {issues.map(issue => (
+          <tr key={issue.id}>
+            {renderFields(issue)}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
